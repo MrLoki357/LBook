87f814afc1715769542ab232d9e94303339ea20d
MrLoki357 <logiloga357@gmail.com>
2021-07-21 20:39:22 +0530

    Splashscreen-finished

--- app/src/main/java/com/example/lbooks/SplashScreen.java
+++ app/src/main/java/com/example/lbooks/SplashScreen.java
@@ -3,12 +3,22 @@ package com.example.lbooks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView SplashImage;
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        SplashImage=findViewById(R.id.img);
+        lottieAnimationView=findViewById(R.id.lottie);
+        SplashImage.animate().translationY(-1800).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
     }
 }
