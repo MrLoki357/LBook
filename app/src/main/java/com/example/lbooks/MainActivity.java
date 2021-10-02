898d929d4ef8738127f575f61313864cf33170cd
MrLoki357 <logiloga357@gmail.com>
2021-10-03 00:43:41 +0530

    fragment sign and fragment register had done

--- app/src/main/java/com/example/lbooks/MainActivity.java
+++ app/src/main/java/com/example/lbooks/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.lbooks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button frag1,frag2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        frag1 = findViewById(R.id.sign);
+        frag2 = findViewById(R.id.reg);
+
+        frag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                replace(new fragment1());
+
+            }
+        });
+        frag2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replace(new fragment2());
+            }
+        });
+
     }
+
+        private void replace(Fragment fragment){
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.framelayout,fragment);
+            fragmentTransaction.commit();
+
+        }
+
 }
--- app/src/main/java/com/example/lbooks/SplashScreen.java
+++ app/src/main/java/com/example/lbooks/SplashScreen.java
@@ -34,7 +34,7 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         SplashImage=findViewById(R.id.img);
         lottieAnimationView=findViewById(R.id.lottie);
-        SplashImage.animate().translationY(-1800).setDuration(1000).setStartDelay(4000);
+        SplashImage.animate().translationY(-2800).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
 
 
