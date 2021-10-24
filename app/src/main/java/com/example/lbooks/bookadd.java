987a44327074aa352c893ceda400081bb6993936
MrLoki357 <logiloga357@gmail.com>
2021-10-24 12:45:03 +0530

    sms and call has been done

--- app/src/main/java/com/example/lbooks/bookadd.java
+++ app/src/main/java/com/example/lbooks/bookadd.java
@@ -2,13 +2,26 @@ package com.example.lbooks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class bookadd extends AppCompatActivity {
-
+Button addbook;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookadd);
+        addbook=findViewById(R.id.bookadd);
+        addbook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Book added Succesfully",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(bookadd.this,mainpage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lbooks/nav_profile.java
+++ app/src/main/java/com/example/lbooks/nav_profile.java
@@ -1,5 +1,7 @@
 package com.example.lbooks;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +9,49 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class nav_profile extends Fragment {
+    LinearLayout callus,smscare;
+    Button logout;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_nav_profile, container, false);
+        callus=view.findViewById(R.id.sendphone);
+        smscare=view.findViewById(R.id.smscare);
+        logout=view.findViewById(R.id.logout);
+        callus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:9445633122"));
+                startActivity(intent);
+            }
+        });
+        smscare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("smsto:9445633122"));
+                intent.putExtra("sms_body","Customer has requested a call");
+                startActivity(intent);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(getActivity(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
